6129689c1e0eba62eae39d9d9aa342233a6c863d
Trevor <trevor.george100@gmail.com>
2019-01-17 07:40:53 -0800

    Update name to depotauto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/depotauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/depotauto.java
@@ -54,7 +54,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="redauto", group="auto")
+@Autonomous(name="depotauto", group="auto")
 public class depotauto extends LinearOpMode {
 
     /* Declare OpMode members. */
